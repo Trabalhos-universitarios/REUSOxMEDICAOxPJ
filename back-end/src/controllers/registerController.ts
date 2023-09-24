00ef153d24fca29bf8cc4d352597d95f4e6bdf24
Johnny Carvalho <Johnny.Carvalho@ibm.com>
2023-09-24 11:30:04 -0300

    Criandoa rquitetura de componentes.

--- back-end/src/controllers/registerController.ts
+++ back-end/src/controllers/registerController.ts
@@ -1,15 +1,24 @@
 import { Response, Request, NextFunction } from "express";
+import prismaClient from "../database/prismaClient";
 
 class RegisterController {
 
     createNewUser = async (req: Request, res: Response, next: NextFunction) => {
 
-        const { user_name, user_adress, user_phone, user_document } = req.body;
+        const { name, specialty, email } = req.body;
 
         console.log('ENTROU NA ROTA API. . .');
         
 
         try {
+
+            const newUser = await prismaClient.user.create({
+                data: {
+                    name,
+                    email,
+                    specialty
+                }
+            })
             
         } catch (error) {
             next(error);

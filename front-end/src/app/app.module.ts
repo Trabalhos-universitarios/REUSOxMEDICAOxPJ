647a281b38f3186ca3aa453d141e29e7534ae263
Johnny Carvalho <Johnny.Carvalho@ibm.com>
2023-09-24 11:30:04 -0300

    Criandoa rquitetura de componentes.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentsComponent } from './components/components.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,

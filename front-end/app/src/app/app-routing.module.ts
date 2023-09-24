a690c7c645c3329dda55cebaaaf989d9edf881a0
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2023-09-24 18:20:18 -0300

    user page creation and landing page completion

--- front-end/app/src/app/app-routing.module.ts
+++ front-end/app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserPageComponent } from './pages/user-page/user-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user-page', component:UserPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/app/src/app/app.module.ts
+++ front-end/app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserPageComponent } from './pages/user-page/user-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserPageComponent
   ],
   imports: [
     BrowserModule,

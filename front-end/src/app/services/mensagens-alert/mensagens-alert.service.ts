094b6ebc7a291ada4d8030e29d8ad80a8487dba0
Johnny Carvalho <Johnny.Carvalho@ibm.com>
2023-09-25 17:12:19 -0300

    Login concluido.

---
+++ front-end/src/app/services/mensagens-alert/mensagens-alert.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { colors } from 'src/colors/colors';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MensagensAlertService {
+
+  constructor() { }
+
+  async mensagemComAcaoAlert(titulo?: string, texto?: string) {
+    return Swal.fire({
+      title: titulo,
+      text: texto,
+      icon: 'warning',
+      iconColor: colors.$principal,
+      showDenyButton: true,
+      confirmButtonColor: colors.$principal,
+      denyButtonColor: '#d33',
+      confirmButtonText: 'CONFIRMAR',
+      denyButtonText: 'CANCELAR'
+    })
+  }
+
+  async mensagemComAcaoAlertTresBotoes(titulo?: string, texto?: string, button1?: string, button2?: string, button3?: string) {
+    return Swal.fire({
+      title: titulo,
+      text: texto,
+      icon: 'warning',
+      iconColor: colors.$aprovado,
+      showCancelButton: true,
+      showDenyButton: true,
+      showConfirmButton: true,
+      cancelButtonText: button1,
+      cancelButtonColor: colors.$vermelho,
+      denyButtonText: button2,
+      denyButtonColor: colors.$cinzaRascunho,
+      confirmButtonText: button3,
+      confirmButtonColor: colors.$aprovado,
+    })
+  }
+
+  async mensagemDeErroAlert(titulo?: string, texto?: string) {
+    await Swal.fire({
+      icon: 'error',
+      title: titulo,
+      text: texto,
+      confirmButtonColor: colors.$vermelho,
+    })
+  }
+
+  async mensagemDeSucessoAlert(titulo?: string) {
+    await Swal.fire({
+      position: 'center',
+      icon: 'success',
+      iconColor: colors.$aprovado,
+      title: titulo,
+      confirmButtonColor: colors.$aprovado,
+    })
+  }
+
+  close() {
+    Swal.close();
+  }
+}

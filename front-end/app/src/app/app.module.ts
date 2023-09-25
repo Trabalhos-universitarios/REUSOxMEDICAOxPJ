5df0042ac942c49ba0440535f3321c9313be2294
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2023-09-25 09:43:12 -0300

    UI, modules creation, and so on

--- front-end/app/src/app/app.module.ts
+++ front-end/app/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserPageComponent } from './pages/user-page/user-page.component';
 import { UnfinishedPagesComponent } from './pages/unfinished-pages/unfinished-pages.component';
+import { CariologiaComponent } from './pages/cariologia/cariologia.component';
+import { OftalmologiaComponent } from './pages/oftalmologia/oftalmologia.component';
+import { OrtopediaComponent } from './pages/ortopedia/ortopedia.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UserPageComponent,
-    UnfinishedPagesComponent
+    UnfinishedPagesComponent,
+    OftalmologiaComponent,
+    OrtopediaComponent,
   ],
   imports: [
     BrowserModule,
